c5d88a227d6e3375da962ed0b0260269507bdeef
Mrk. <m4rk1vmn@gmail.com>
2026-05-22 01:14:15 +0200

    default hu

--- config/config.lua
+++ config/config.lua
@@ -1,6 +1,6 @@
 Wolfy = {}
 
-Wolfy.Language = "en" -- Hu or En
+Wolfy.Language = "hu" -- Hu or En
 Wolfy.ResourceAllowed = { -- List of resources that are allowed to use the exports
     ['esx_society'] = true,
     ['esx_advancedgarage'] = true
